ff08f87155c144d36de4f0d76be0a7ea5aeee490
jace lee <javakotlinspring@gmail.com>
2023-12-22 10:27:42 +0900

    second commit

--- src/HangHae99/No2.java
+++ src/HangHae99/No2.java
@@ -2,11 +2,10 @@ package HangHae99;
 
 import java.util.Scanner;
 
-public class No1 {
+public class No2 {
     public static void main(String[] args) {
-        // 정수 num1과 num2가 주어질 때, num1에서 num2를 뺀 값을 return하도록 solution함수 완성
-        // 문제 URL : https://school.programmers.co.kr/learn/courses/30/lessons/120803
-        // raw code :
+        // 정수 num1, num2가 매개변수 주어집니다. num1과 num2를 곱한 값을 return하도록 solution함수를 완성
+        // 문제 URL : https://school.programmers.co.kr/learn/courses/30/lessons/120804
         Scanner sc = new Scanner(System.in);
         int num1 = sc.nextInt();
         int num2 = sc.nextInt();
@@ -15,7 +14,7 @@ public class No1 {
     }
 
     static int solution(int num1, int num2) {
-        int answer = num1 - num2;
+        int answer = num1*num2;
         return answer;
     }
 }
--- src/HangHae99/No3.java
+++ src/HangHae99/No3.java
@@ -2,11 +2,11 @@ package HangHae99;
 
 import java.util.Scanner;
 
-public class No1 {
+public class No3 {
     public static void main(String[] args) {
-        // 정수 num1과 num2가 주어질 때, num1에서 num2를 뺀 값을 return하도록 solution함수 완성
-        // 문제 URL : https://school.programmers.co.kr/learn/courses/30/lessons/120803
-        // raw code :
+        // 정수 num1, num2가 매개변수로 주어질 때, num1을 num2로 나눈 몫을 return하도록
+        // solution함수를 완성
+        // 문제 URL : https://school.programmers.co.kr/learn/courses/30/lessons/120805
         Scanner sc = new Scanner(System.in);
         int num1 = sc.nextInt();
         int num2 = sc.nextInt();
@@ -15,7 +15,7 @@ public class No1 {
     }
 
     static int solution(int num1, int num2) {
-        int answer = num1 - num2;
+        int answer = num1/num2;
         return answer;
     }
 }
